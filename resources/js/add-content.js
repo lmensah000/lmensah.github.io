51aeec040e97534f8529b2038b1c143d49589ee6
Lawrence Mensah <lmensah000@gmail.com>
2019-08-12 15:24:56 -0400

    slight edits added into contact form

--- resources/js/add-content.js
+++ resources/js/add-content.js
@@ -35,3 +35,13 @@ function imagesGreeting() {
     document.getElementById('bitmoji').src = images[IG];
     document.getElementById('bitmoji').alt = images[IG];
 }
+
+function ValidateEmail(mail)
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
+  {
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
