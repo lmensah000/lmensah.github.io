786e7b6271cd8ffbb12fca1c3b79b61fdc89ae3f
Lawrence Mensah <lmensah000@gmail.com>
2019-08-12 02:05:14 -0400

    New updates installed
    Sticky nav
    changing bitmoji greeting in nav
    new background images saved and installed

--- resources/js/add-content.js
+++ resources/js/add-content.js
@@ -1,14 +1,37 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > 550) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
 //Adding logo that changes based on time
 //Using javascript and jquery
-var today = new Date();
-var hourNow = today.getHours();
+var images = [
+  "resources/images/hellomoji.jpeg",
+  "resources/images/eveningmoji.jpeg",
+  "resources/images/afternoonmoji.jpeg",
+  "resources/images/morningmoji.jpeg"
+  ];
 
-if (hourNow >= 18) {
-  document.write('<img src="resources/images/eveningmoji.jpeg">');
-}
-else if (hourNow >= 12 && hourNow < 18) {
-  document.write('<img src="resources/images/afternoonmoji.jpeg">');
-}
-else if(hourNow > 0 &&  hourNow < 12) {
-  document.write('<img src="resources/images/morningmoji.jpeg">');
+function imagesGreeting() {
+  var today = new Date();
+  hourNow = today.getHours();
+  var IG = 0;
+    if (hourNow >= 18) {IG = 1; }
+    else if((hourNow >= 12) && (hourNow < 18)) {IG = 2; }
+    else if((hourNow > 0) &&  (hourNow < 12)) {IG = 3; }
+    document.getElementById('bitmoji').src = images[IG];
+    document.getElementById('bitmoji').alt = images[IG];
 }
